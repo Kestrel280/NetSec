52360c13cfc703289d0c015c2a64c23e4549d73f
Sam Downey <Sam_Downey@student.uml.edu>
2025-04-03 20:47:24 +0000

    Project2 adds start of Diffie-Hellman key exchange for WMCP

--- Project2/agent.py
+++ Project2/agent.py
@@ -9,11 +9,16 @@ from utils import * # sha3(), gcd(), fme(), mmi(), is_prime()
 # Defined constants
 LISTEN_PORT = 10176
 NONCE_SIZE_BYTES = 16
-NBUF_SIZE = 1024
+NBUF_SIZE = 4096        # Buffer size in bytes for socket.recv() calls; DH sends 4096-bit numbers using base16 repr = 1024 chars, so should be at least 2x that
+
+#RFC 3526: 4096-bit mod-p group
+DH_G = 2
+DH_P = int("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AAAC42DAD33170D04507A33A85521ABDF1CBA64ECFB850458DBEF0A8AEA71575D060C7DB3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D2261AD2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200CBBE117577A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFCE0FD108E4B82D120A92108011A723C12A787E6D788719A10BDBA5B2699C327186AF4E23C1A946834B6150BDA2583E9CA2AD44CE8DBBBC2DB04DE8EF92E8EFC141FBECAA6287C59474E6BC05D99B2964FA090C3A2233BA186515BE7ED1F612970CEE2D7AFB81BDD762170481CD0069127D5B05AA993B4EA988D8FDDC186FFB7DC90A6C08F4DF435C934063199FFFFFFFFFFFFFFFF", 16)
 
 # Global variables
 # sjt = None      # Secret join token
 sjt = "abc123"
+wid = 1         # id to assign to next connected worker
 
 # Arguments
 parser = argparse.ArgumentParser()
@@ -40,21 +45,33 @@ def handle_new_connection(sock, addr):
     Communicates with a newly connected entity to determine what they want.
     Dispatches to appropriate handler.
     """
-    print(f"in handle_new_connection with sock={sock}, addr={addr}")
+    print(f"M: in handle_new_connection with sock={sock}, addr={addr}")
 
     imsg = sock.recv(NBUF_SIZE).decode('utf-8')
 
-    print(f"mgr received imsg = '{imsg}'")
-
+    print(f"M: received imsg = '{imsg}'")
     cmd = imsg.split(' ')[0]
     try:
         match cmd:
             case 'register':
-                # Worker-Manager Connection Protocol, step 1 validation
-                Na, sig = imsg.split(' ')[1:3]
+                # --- Worker-Manager Connection Protocol --- 
+                # Worker has sent step 1: check it's a unique nonce, and that they signed using the SJT
+                Na, isig = imsg.split(' ')[1:3]
+                # TODO add this nonce to used nonce list
+                # TODO check if this nonce has already been used
                 tsig = sha3(f"{cmd} {Na}{sjt}")
-                assert tsig == sig
-                # Step  = os.urandom(NONCE_SIZE_BYTES).hex()
+                assert tsig == isig
+
+                # Step 2: Generate a nonce and DH parameter, and send to worker
+                Nm = os.urandom(NONCE_SIZE_BYTES).hex()
+                # TODO add this nonce to used nonce list
+                x = random.randint(2**1024, 2**4095) # Generate my Diffie-Hellman parameter
+                gxmodp = fme(DH_G, x, DH_P)
+
+                omsg = f"{wid} {Nm} {gxmodp:x}" # [id, Nm (hex), g^x mod p (hex)]
+                osig = sha3(f"{omsg}{sjt}")
+                sock.send(f"{omsg} {osig}".encode('utf-8'))
+
             case 'list_agents':
                 # TODO do Cluster Services Connection Protocol
                 # TODO return list of workers
@@ -65,7 +82,6 @@ def handle_new_connection(sock, addr):
                 pass
     except AssertionError:
         print("worker provided invalid signature, terminating their connection request")
-        sock.send("invalid_signature".encode('utf-8'))
         sock.close()
 
     return
@@ -161,22 +177,32 @@ def connect_to_manager(mip):
         Nothing (for now)
     """
 
-    print(f"in connect_to_manager() with mip={mip}")
+    print(f"W: in connect_to_manager() with mip={mip}")
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
     sock.settimeout(3)
     sock.connect((mip, LISTEN_PORT))
 
-    print(f"worker connected to manager at ip {mip}")
+    print(f"W: worker connected to manager at ip {mip}")
+    # --- Worker-Manager Connection Protocol ---
+    try:
+        # Step 1: generate a nonce and send a connection request to manager, signed using SJT
+        Na = os.urandom(NONCE_SIZE_BYTES).hex()
+        # TODO add this nonce to used nonce list
+        omsg = f"register {Na}"
+        osig = sha3(f"{omsg}{sjt}")
+        sock.send(f"{omsg} {osig}".encode('utf-8'))
 
-    # Worker-Manager Connection Protocol (worker's side)
-    Na = os.urandom(NONCE_SIZE_BYTES).hex()
-    omsg = f"register {Na}"
-    print(f"worker calculating sha3 of '{omsg}{sjt}'")
-    sig = sha3(f"{omsg}{sjt}")
-    print(f"omsg: {omsg}")
-    print(f"sig: {sig}")
-    sock.send(f"{omsg} {sig}".encode('utf-8'))
+        # Manager sends back step 2
+        imsg = sock.recv(NBUF_SIZE).decode('utf-8')
+        print(f"W: received imsg = '{imsg}'")
+        wid, Nm, gxmodp, isig = imsg.split(' ')
+        # TODO add this nonce to used nonce list
+        tsig = sha3(f"{wid} {Nm} {gxmodp}{sjt}")
+        assert tsig == isig
+    except AssertionError:
+        print("W: manager provided invalid signature, terminating connection request")
+        sock.close()
 
     return
 
