33354adab6f5bb1ccce5422514c7f3501e2e06df
Sam Downey <Sam_Downey@student.uml.edu>
2025-04-19 01:50:32 +0000

    Project2 bootstrap manager now creates an SJT

--- Project2/agent.py
+++ Project2/agent.py
@@ -10,10 +10,10 @@ from utils import * # sha3(), gcd(), fme(), mmi(), is_prime()
 
 # Defined constants
 PRINT_DEBUG         = True
-PRINT_MSGS          = True
+PRINT_MSGS          = False
 PRINT_REGISTRATION  = True
-PRINT_REG_LISTS     = True  # When a new node/nonce is registered, print the full list
-LISTEN_PORT = 10174
+PRINT_REG_LISTS     = False  # When a new node/nonce is registered, print the full list
+LISTEN_PORT = 10176
 NONCE_SIZE_BYTES = 16
 HEARTBEAT_INTERVAL = 5
 fmt_mgr = "mgr    "
@@ -23,8 +23,7 @@ DH_G = 2
 DH_P = int("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AAAC42DAD33170D04507A33A85521ABDF1CBA64ECFB850458DBEF0A8AEA71575D060C7DB3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D2261AD2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200CBBE117577A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFCE0FD108E4B82D120A92108011A723C12A787E6D788719A10BDBA5B2699C327186AF4E23C1A946834B6150BDA2583E9CA2AD44CE8DBBBC2DB04DE8EF92E8EFC141FBECAA6287C59474E6BC05D99B2964FA090C3A2233BA186515BE7ED1F612970CEE2D7AFB81BDD762170481CD0069127D5B05AA993B4EA988D8FDDC186FFB7DC90A6C08F4DF435C934063199FFFFFFFFFFFFFFFF", 16)
 
 # Global variables
-# sjt = None              # Secret join token
-sjt = "abc123"
+sjt = None              # Secret join token
 wid = 0                 # running id counter for newly connected workers
 used_nonces = set()     # Set of used nonces
 network_nodes = {}      # List of Nodes on the cluster, connected or not
@@ -122,7 +121,7 @@ def handle_user_request(sock, addr, init_msg):
             sock.send(omsg)
         
     except Exception as e:
-        print(f"Manager in handle_user_reuqest encountered exception {e}")
+        print(f"Manager in handle_user_request encountered exception {e}")
 
     finally:
         # Close the connection when done
@@ -139,6 +138,7 @@ def handle_worker(sock, addr, init_msg):
     global wid
     wid += 1
     twid = wid
+    node = None
 
     # Keep a list of things to do if we need to terminate connection
     _cleanup = [lambda: sock.close()] # On cleanup, will need to close socket
@@ -206,10 +206,11 @@ def handle_worker(sock, addr, init_msg):
                         node.busy = False
             imsg = node.secure_recv()
     except Exception as e:
-        print(f"{fmt_mgr} with worker {node.nid:5} encountered exception {e}")
+        print(f"{fmt_mgr} encountered exception with worker {twid}: {e}")
     finally:
-        print(f"{fmt_mgr} closing connection to worker {node.nid:5}")
-        handle_worker_unavailability(node)
+        print(f"{fmt_mgr} closing connection to worker {twid}")
+        if node:
+            handle_worker_unavailability(node)
         for fn in _cleanup:
             try: fn()
             except Exception as e: print(f"{fmt_mgr} encountered exception during cleanup of worker {twid}: {e}")
@@ -383,8 +384,10 @@ def become_manager():
     Returns:
         Nothing (for now)
     """
-    if (sjt is None):
-        # TODO generate sjt
+    global sjt
+    if sjt is None:
+        sjt = os.urandom(16).hex()
+        print(f"\n\nM: SJT is:\n{sjt}\n\n")
         pass
 
     start_connection_listener()
@@ -491,13 +494,13 @@ def connect_to_manager(mip):
             imsg = mgr.secure_recv()
 
     except AssertionError:
-        print("w {twid:5} provided invalid signature, terminating connection")
+        print(f"w {twid:5} received invalid signature, terminating connection attempt")
+    except ValueError:
+        print(f"w {twid:5} was rejected by manager for invalid signature, terminating connection attempt")
     except Exception as e:
         print(f"w {twid:5} encountered exception {e}")
     finally:
-        if twid < 0:
-            print("worker failed to initialize! could not connect to manager")
-            return
+        if twid < 0: return
         print(f"w {twid:5} closing connection to manager")
         for fn in _cleanup:
             try: fn()
@@ -674,7 +677,7 @@ if __name__ == '__main__':
     if args.bootstrap:
         become_manager()
     elif ((args.join is not None) and (args.token is not None)):
-        # sjt = args.token[0] TODO uncomment when we're actually setting an sjt; don't need to set it as global since we're still in global namespace here
+        sjt = args.token[0]
         connect_to_manager(args.join[0])
     elif ((args.deploy_service is not None) and (args.path is not None)):
         deploy_service(args.deploy_service[0], args.path[0])
